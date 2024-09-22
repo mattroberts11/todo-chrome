6016303529f5304c845657f90e598c4a35842046
Matt Roberts <justskiit@gmail.com>
2024-09-22 12:25:15 -0600

    chore: built out new light dark mode toggle

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,33 +1,85 @@
 import React from "react";
 
+// Declare chrome as a global variable
+declare const chrome: {
+  storage: {
+    sync: {
+      get: (
+        key: string,
+        callback: (data: { [key: string]: string }) => void,
+      ) => void;
+    };
+  };
+};
+
 import { useColorScheme } from "@mui/joy/styles";
-import { Select, Option } from "@mui/joy";
+import { Switch } from "@mui/joy";
+
+import LightModeIcon from "@mui/icons-material/LightMode";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
 
 const ModeToggle: React.FC = () => {
   const { mode, setMode } = useColorScheme();
+
   const [mounted, setMounted] = React.useState(false);
 
-  // necessary for server-side rendering
-  // because mode is undefined on the server
   React.useEffect(() => {
-    setMounted(true);
-  }, []);
+    // check if chrome.storage.sync is available to set theme
+    if (
+      typeof chrome !== "undefined" &&
+      chrome.storage &&
+      chrome.storage.sync
+    ) {
+      chrome.storage.sync.get("theme", (data) => {
+        const storedTheme: "light" | "dark" | null =
+          data.theme === "dark" ? "dark" : "light"; // default to light if not set
+        setMode(storedTheme);
+        // necessary for server-side rendering
+        // because mode is undefined on the server
+        setMounted(true);
+      });
+    } else {
+      // Fallback to system preference if chrome.storage.sync is not available
+      const systemPrefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)",
+      ).matches;
+      setMode(systemPrefersDark ? "dark" : "light");
+      setMounted(true);
+    }
+  }, [setMode]);
   if (!mounted) {
     return null;
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newMode = event.target.checked ? "dark" : "light";
+    setMode(newMode);
+  };
+
   return (
-    <Select
-      value={mode}
-      onChange={(event, newMode) => {
-        setMode(newMode);
-      }}
-      sx={{ width: "max-content" }}
-    >
-      <Option value="system">System</Option>
-      <Option value="light">Light</Option>
-      <Option value="dark">Dark</Option>
-    </Select>
+    <Switch
+      color={mode === "dark" ? "primary" : "success"}
+      size="sm"
+      slotProps={{ input: { "aria-label": "dark mode" } }}
+      startDecorator={
+        <LightModeIcon
+          sx={{
+            color: mode === "dark" ? "text.tertiary" : "success.500",
+            width: "16px",
+          }}
+        />
+      }
+      endDecorator={
+        <ModeNightIcon
+          sx={{
+            color: mode === "dark" ? "primary.500" : "text.tertiary",
+            width: "16px",
+          }}
+        />
+      }
+      checked={mode === "dark"}
+      onChange={handleChange}
+    />
   );
 };
 
