8063a17c4184ac1197015d08b6f878bd2a0e7c86
Matt Roberts <justskiit@gmail.com>
2024-09-29 16:17:40 -0600

    chore: setup files

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.action.setBadgeText({
+//     text: "OFF",
+//   });
+// });
