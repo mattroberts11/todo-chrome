289e6d81b04c59c4b87b0c15be155cd9a47aa6c7
Matt Roberts <justskiit@gmail.com>
2024-10-06 16:03:30 -0600

    chore: list inline editing and some animation

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { AppContext } from "../AppContext";
-import { Box, Checkbox, IconButton, List, ListItem } from "@mui/joy";
+import {
+  Box,
+  Checkbox,
+  IconButton,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/joy";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { useTheme } from "@mui/joy";
+import { motion, AnimatePresence } from "framer-motion";
 
 import TaskInput from "./TaskInput";
 
@@ -10,24 +19,19 @@ const TodoList: React.FC = () => {
   const theme = useTheme();
 
   const { state, dispatch } = React.useContext(AppContext);
-  const [isEditing, setIsEditing] =
-    React.useState<React.SetStateAction<boolean>>(false);
+  const [editingId, setEditingId] = React.useState<string | null>(null);
+  const [hoveredId, setHoveredId] = React.useState<string | null>(null);
 
-  const todoTextRef = React.useRef<HTMLDivElement>(null);
   const inputRef = React.useRef<HTMLInputElement>(null);
-  console.log("TodoList render, state:", state);
 
   const handleDelete = (id: string) => {
-    console.log("deleted todo with id: ", id);
     dispatch({ type: "DELETE_TODO", payload: id });
   };
 
-  // Function to update a single todo item in chrome.storage.sync
   const updateTodoInStorage = (
     updatedTodo: ToDo,
     updatingCompleted: boolean,
   ) => {
-    // First, get the current data from storage
     chrome.storage.sync.get(updatedTodo.id, (result) => {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError);
@@ -35,22 +39,15 @@ const TodoList: React.FC = () => {
       }
 
       if (updatingCompleted) {
-        // Toggles the completed status of the todo
         updatedTodo = { ...updatedTodo, completed: !updatedTodo.completed };
       }
-      // Check if the item exists in storage
       if (result[updatedTodo.id]) {
-        // Merge the updated todo with the existing data
         const mergedTodo = { ...result[updatedTodo.id], ...updatedTodo };
-
-        // Create an object with the updated todo
         const updatedData = {
           [updatedTodo.id]: mergedTodo,
         };
 
-        // Set the updated data back to storage
         chrome.storage.sync.set(updatedData, () => {
-          console.log("Updated todo:", updatedData);
           if (chrome.runtime.lastError) {
             console.error(chrome.runtime.lastError);
           } else {
@@ -65,87 +62,149 @@ const TodoList: React.FC = () => {
 
   const handleCheck = (todo: ToDo) => {
     const updatedTodo = { ...todo, completed: !todo.completed };
-    console.log("checked todo with todo: ", updatedTodo);
     updateTodoInStorage(todo, true);
     dispatch({ type: "UPDATE_TODO", payload: updatedTodo });
   };
 
-  const handleEdit = (todo: ToDo) => {
-    console.log("Editing todo: ", todo);
-    setIsEditing(true);
+  const handleEdit = (todoId: string) => {
+    setEditingId(todoId);
   };
 
-  return (
-    <List>
-      {state.todoStorage && state.todoStorage.length > 0 ? (
-        state.todoStorage.map((todo, i) => (
-          <React.Fragment key={`${todo.id}-${i}`}>
-            <ListItem
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                background: theme.palette.background.level3,
-                borderRadius: "8px",
-                padding: "10px",
-                marginBottom: "8px",
-              }}
-            >
-              <Checkbox
-                className="todo-checkbox"
-                size="md"
-                color="primary"
-                variant="outlined"
-                sx={{
-                  marginRight: "5px",
-                }}
-                checked={todo.completed}
-                onChange={() => handleCheck(todo)}
-              ></Checkbox>
+  const handleEditSave = (todoId: string, newText: string) => {
+    const todoToUpdate = state.todoStorage.find((todo) => todo.id === todoId);
+    if (todoToUpdate) {
+      const updatedTodo = { ...todoToUpdate, text: newText };
+      updateTodoInStorage(updatedTodo, false);
+      dispatch({ type: "UPDATE_TODO", payload: updatedTodo });
+    }
+    setEditingId(null);
+  };
+
+  const renderTodoItem = (todo: ToDo) => (
+    <motion.div
+      layout
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.3 }}
+      key={todo.id}
+    >
+      <ListItem
+        key={`${todo.id}`}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          background: theme.palette.background.level3,
+          borderRadius: "8px",
+          padding: "10px",
+          marginBottom: "8px",
+        }}
+        component={motion.li}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        exit={{ opacity: 0 }}
+      >
+        <Checkbox
+          className="todo-checkbox"
+          size="md"
+          color="primary"
+          variant="outlined"
+          sx={{
+            marginRight: "5px",
+          }}
+          checked={todo.completed}
+          onChange={() => handleCheck(todo)}
+        />
+        <Box
+          className="todo-text-wrapper"
+          sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+        >
+          <Box
+            className="todo-text"
+            sx={{
+              overflowWrap: "anywhere",
+              "&:hover": { cursor: "pointer" },
+            }}
+            onClick={() => handleEdit(todo.id)}
+            onMouseEnter={() => setHoveredId(todo.id)}
+            onMouseLeave={() => setHoveredId(null)}
+          >
+            {editingId === todo.id ? (
+              <TaskInput
+                initialValue={todo.text}
+                inputVariant="plain"
+                showButtonsUnderInput={false}
+                inputRef={inputRef}
+                onSave={(newText) => handleEditSave(todo.id, newText)}
+              />
+            ) : (
               <Box
-                className="todo-text-wrapper"
-                sx={{ display: "flex", flexDirection: "column" }}
+                component={"span"}
+                sx={{
+                  textDecoration: todo.completed ? "line-through" : "none",
+                }}
               >
-                <Box
-                  className="todo-text"
-                  sx={{
-                    overflowWrap: "anywhere",
-                  }}
-                  onClick={() => handleEdit(todo)}
-                  ref={todoTextRef}
-                >
-                  {isEditing ? (
-                    <TaskInput
-                      initialValue={todo.text}
-                      inputVariant="plain"
-                      showButtonsUnderInput={false}
-                      inputRef={inputRef}
-                    />
-                  ) : (
-                    <Box
-                      component={"span"}
-                      sx={{
-                        textDecoration: todo.completed
-                          ? "line-through"
-                          : "none",
-                      }}
-                    >
-                      {String(todo.text)}
-                    </Box>
-                  )}
-                </Box>
+                {String(todo.text)}
               </Box>
-              <Box className="todo-delete" sx={{ marginLeft: "auto" }}>
-                <IconButton variant="plain">
-                  <DeleteIcon onClick={() => handleDelete(todo.id)} />
-                </IconButton>
-              </Box>
-            </ListItem>
-          </React.Fragment>
-        ))
-      ) : (
-        <ListItem>No todos available</ListItem>
+            )}
+          </Box>
+        </Box>
+        <Box className="todo-delete">
+          <IconButton
+            variant="plain"
+            onClick={() => handleEdit(todo.id)}
+            sx={{
+              display: hoveredId === todo.id ? "block" : "none",
+            }}
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            variant="plain"
+            onClick={() => handleDelete(todo.id)}
+            sx={{
+              display: hoveredId === todo.id ? "none" : "block",
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      </ListItem>
+    </motion.div>
+  );
+
+  const activeTodos = state.todoStorage.filter((todo) => !todo.completed);
+  const completedTodos = state.todoStorage.filter((todo) => todo.completed);
+
+  return (
+    <Box>
+      <Typography level="h4" sx={{ marginBottom: "16px" }}>
+        Active Tasks
+      </Typography>
+      <AnimatePresence>
+        <List>
+          {activeTodos.length > 0 ? (
+            activeTodos.map(renderTodoItem)
+          ) : (
+            <ListItem>No active todos available</ListItem>
+          )}
+        </List>
+      </AnimatePresence>
+
+      {completedTodos.length > 0 && (
+        <>
+          <Typography
+            level="h4"
+            sx={{ marginTop: "24px", marginBottom: "16px" }}
+          >
+            Completed Tasks
+          </Typography>
+          <AnimatePresence>
+            <List>{completedTodos.map(renderTodoItem)}</List>
+          </AnimatePresence>
+        </>
       )}
-    </List>
+    </Box>
   );
 };
 
